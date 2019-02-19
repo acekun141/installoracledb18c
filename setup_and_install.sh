29922519a8223e26ddb8e63da3d48b5d81c2fd32
unknown <acekun141@gmail.com>
2019-02-19 11:02:17 +0700

    .

--- setup_and_install.sh
+++ setup_and_install.sh
@@ -27,5 +27,5 @@ wget https://raw.githubusercontent.com/acekun141/oracle18c/master/packages/oracl
 wget https://raw.githubusercontent.com/acekun141/oracle18c/master/packages/oracle-database-xe-18c-1.0-1.x86_64.zip.026
 wget https://raw.githubusercontent.com/acekun141/oracle18c/master/packages/oracle-database-xe-18c-1.0-1.x86_64.zip.027
 wget https://raw.githubusercontent.com/acekun141/oracle18c/master/packages/oracle-database-xe-18c-1.0-1.x86_64.zip.028
-sudo apt-get install p7zip-full
+
 7z x oracle-database-xe-18c-1.0-1.x86_64.zip.001
